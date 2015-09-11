e31c05dea3aad6d8908b5096b39c76e3282c38fb
Pras Velagapudi <psigen@gmail.com>
2015-09-11 10:51:01 -0400

    Started to lay out chimera structure.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# kludge #
+# Chimera #
 
-> ####/klo͞oj/ [![speaker][2]][1]
+> #### chi·me·ra ####
+> #### kīˈmirə,kəˈmirə/ #### [![speaker][2]][1]
 > _*informal*, noun_
 >
-> 1. an ill-assorted collection of parts assembled to fulfill a particular purpose.
+> 1. a thing that is hoped or wished for but in fact is illusory or impossible to achieve.
 > 2. a utility to generate Boost.Python bindings for C++ code.
 
-[1]: https://ssl.gstatic.com/dictionary/static/sounds/de/0/kludge.mp3
+[1]: http://www.oxforddictionaries.com/us/media/american_english/us_pron_ogg/c/chi/chime/chimera__us_1_rr.ogg
 [2]: https://upload.wikimedia.org/wikipedia/commons/7/74/Speaker_icon.svg
--- src/chimera.cpp
+++ src/chimera.cpp
@@ -1,5 +1,5 @@
 /**
- * Example test file.
+ * Chimera - a tool to convert c++ headers into Boost.Python bindings.
  */
 
 // Declares clang::SyntaxOnlyAction.
@@ -15,7 +15,7 @@ using namespace llvm;
 
 // Apply a custom category to all command-line options so that they are the
 // only ones displayed.
-static cl::OptionCategory MyToolCategory("my-tool options");
+static cl::OptionCategory ChimeraCategory("chimera options");
 
 // CommonOptionsParser declares HelpMessage with a description of the common
 // command-line options related to the compilation database and input files.
@@ -23,10 +23,10 @@ static cl::OptionCategory MyToolCategory("my-tool options");
 static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 
 // A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...");
+static cl::extrahelp MoreHelp("\nChimera is a conversion tool");
 
 int main(int argc, const char **argv) {
-  CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+  CommonOptionsParser OptionsParser(argc, argv, ChimeraCategory);
   ClangTool Tool(OptionsParser.getCompilations(),
                  OptionsParser.getSourcePathList());
   return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
