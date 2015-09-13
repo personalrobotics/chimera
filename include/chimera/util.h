353470fdf00d2079ab0d6183dd946f8a5bb56225
Pras Velagapudi <psigen@gmail.com>
2015-09-13 02:52:50 -0400

    Added docstrings for the fairly confusing util functions.

--- include/chimera/util.h
+++ include/chimera/util.h
@@ -10,9 +10,29 @@ namespace chimera
 namespace util
 {
 
+/**
+ * Resolve a declaration string within the scope of a compiler instance.
+ *
+ * This function parses the provided declaration string as a single line
+ * of the form `[declStr];` within the AST that is currently loaded by
+ * the provided compiler instance.
+ *
+ * If it can be resolved to a named declaration, the canonical clang::Decl
+ * pointer associated with the declaration will be returned, otherwise NULL.
+ */
 const clang::NamedDecl* resolveDeclaration(clang::CompilerInstance *ci,
                                            const llvm::StringRef declStr);
 
+/**
+ * Resolve a namespace string within the scope of a compiler instance.
+ *
+ * This function parses the provided namespace string as a single line
+ * of the form `namespace [nsStr] {};` within the AST that is currently
+ * loaded by the provided compiler instance.
+ *
+ * If it can be resolved to a named declaration, the canonical clang::Decl
+ * pointer associated with the namespace will be returned, otherwise NULL.
+ */
 const clang::NamedDecl* resolveNamespace(clang::CompilerInstance *ci,
                                          const llvm::StringRef nsStr);
 
