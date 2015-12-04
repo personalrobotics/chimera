87ce61bc5b65449da4ce79db0aaae91d2acca7c1
Michael Koval <mkoval@cs.cmu.edu>
2015-12-04 00:49:34 -0500

    More suppressions for DART.

--- test/bind_dart.sh
+++ test/bind_dart.sh
@@ -4,3 +4,4 @@ ARGS="-DBOOST_TEST_DYN_LINK -Wall -msse2 -fPIC -std=c++11 -O3 -DNDEBUG -I${DART_
 
 mkdir -p /tmp/dartpy
 ../build/chimera -m dartpy -c ./test.yaml -o /tmp/dartpy "${DART_INCLUDE}/dart/dart.h" -- ${ARGS}
+cp CMakeLists.txt /tmp/dartpy
