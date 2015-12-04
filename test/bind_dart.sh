db065a235e8e6745939c47b7d2cdd485fcf1a8dd
Michael Koval <mkoval@cs.cmu.edu>
2015-12-04 00:27:27 -0500

    Tweaks to DART bind script.

--- test/bind_dart.sh
+++ test/bind_dart.sh
@@ -3,4 +3,4 @@ DART_INCLUDE="${HOME}/storage/dart-ws/devel/include"
 ARGS="-DBOOST_TEST_DYN_LINK -Wall -msse2 -fPIC -std=c++11 -O3 -DNDEBUG -I${DART_INCLUDE} -isystem /usr/include/eigen3 -isystem /opt/ros/indigo/include -isystem /usr/include/coin -isystem /home/mkoval/storage/chimera-ws/devel/lib/clang/3.8.0/include/"
 
 mkdir -p /tmp/dartpy
-../build/chimera -c ./test.yaml -o /tmp/dartpy "${DART_INCLUDE}/dart/dart.h" -- ${ARGS}
+../build/chimera -m dartpy -c ./test.yaml -o /tmp/dartpy "${DART_INCLUDE}/dart/dart.h" -- ${ARGS}
