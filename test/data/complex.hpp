24513fc135aabf2536ed724f4e5699531caff50f
Pras Velagapudi <psigen@gmail.com>
2015-12-10 10:52:08 -0500

    Squashed 'external/mstch/' content from commit 0f1a705
    git-subtree-dir: external/mstch
    git-subtree-split: 0f1a7056c10c844bfb6e2a659e7f56d8dc3d3c1a

---
+++ test/data/complex.hpp
@@ -0,0 +1,69 @@
+class complex_item: public mstch::object {
+private:
+  std::string m_name;
+  bool m_current;
+  std::string m_url;
+public:
+  complex_item(const std::string& name, bool current, const std::string& url):
+      m_name(name), m_current(current), m_url(url)
+  {
+    register_methods(this, std::map<std::string,mstch::node(complex_item::*)()>{
+      {"name", &complex_item::name}, {"current", &complex_item::current},
+      {"url", &complex_item::url},   {"link", &complex_item::link}
+    });
+  }
+
+  mstch::node current() {
+    return m_current;
+  }
+
+  mstch::node url() {
+    return m_url;
+  }
+
+  mstch::node name() {
+    return m_name;
+  }
+
+  mstch::node link() {
+    return !m_current;
+  }
+};
+
+class complex: public mstch::object {
+private:
+  std::string m_header;
+  mstch::array m_item;
+public:
+  complex():
+      m_header("Colors"),
+      m_item(mstch::array{
+        std::make_shared<complex_item>("red", true, "#Red"),
+        std::make_shared<complex_item>("green", false, "#Green"),
+        std::make_shared<complex_item>("blue", false, "#Blue")
+      })
+  {
+    register_methods(this, std::map<std::string,mstch::node(complex::*)()>{
+      {"header", &complex::header}, {"item", &complex::item},
+      {"list", &complex::list}, {"empty", &complex::empty}
+    });
+  }
+
+  mstch::node header() {
+    return m_header;
+  }
+
+  mstch::node item() {
+    return m_item;
+  }
+
+  mstch::node list() {
+    return m_item.size() != 0;
+  }
+
+  mstch::node empty() {
+    return m_item.size() == 0;
+  }
+};
+
+const auto complex_data = std::make_shared<complex>();
---
+++ test/data/simple.hpp
@@ -0,0 +1,32 @@
+class simple: public mstch::object {
+private:
+  int m_value;
+public:
+  simple():
+    m_value(10000)
+  {
+    register_methods(this, std::map<std::string,mstch::node(simple::*)()>{
+      {"name", &simple::name},
+      {"value", &simple::value},
+      {"taxed_value", &simple::taxed_value},
+      {"in_ca", &simple::in_ca}});
+  }
+  
+  mstch::node name() {
+    return std::string{"Chris"};
+  }
+
+  mstch::node value() {
+    return m_value;
+  }
+
+  mstch::node taxed_value() {
+    return m_value - (m_value * 0.4);
+  }
+
+  mstch::node in_ca() {
+    return true;
+  }
+};
+
+const auto simple_data = std::make_shared<simple>();
