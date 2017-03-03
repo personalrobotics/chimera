9358c0476e177df44b321588a945d6120268341e
Jeongseok Lee <jslee02@gmail.com>
2017-03-03 16:45:32 -0500

    Fix build script for macOS

--- ci/install_macos.sh
+++ ci/install_macos.sh
@@ -1,2 +1,3 @@
 brew install yaml-cpp --with-static-lib
-PKG_CONFIG_PATH=/usr/local/Cellar/yaml-cpp/0.5.2/lib/pkgconfig cmake -DLLVM_DIR=/usr/local/opt/llvm/share/llvm/cmake ..
+PKG_CONFIG_PATH=/usr/local/Cellar/yaml-cpp/0.5.2/lib/pkgconfig 
+
