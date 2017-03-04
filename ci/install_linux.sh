248f93c75496f5452b4d1780a9cb29991d18799d
Jeongseok Lee <jslee02@users.noreply.github.com>
2017-03-03 19:20:34 -0500

    Update install_linux.sh

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -1,4 +1,4 @@
 sudo add-apt-repository 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.6 main' -y
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-get update -q
-sudo apt-get install llvm-3.6-dev llvm-3.6-tools libclang-3.6-dev libedit-dev libyaml-cpp-dev libboost-all-dev -y
+sudo apt-get install llvm-3.6-dev llvm-3.6-tools libclang-3.6-dev libedit-dev libyaml-cpp-dev libboost-dev -y
