10957f089aa1abc6e2d0a02ac5b89b1513d6ecb8
Michael Koval <mkoval@cs.cmu.edu>
2017-03-16 23:32:40 -0400

    Update Travis for macOS.

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -1,4 +1,4 @@
-sudo add-apt-repository 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.6 main' -y
+sudo add-apt-repository -y "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-${LLVM_VERSION} main"
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo apt-get update -q
-sudo apt-get install llvm-3.6-dev llvm-3.6-tools libclang-3.6-dev libedit-dev libyaml-cpp-dev libboost-dev -y
+sudo apt-get -q update
+sudo apt-get -y install "llvm-${LLVM_VERSION}-dev" "llvm-${LLVM_VERSION}-tools" "libclang-${LLVM_VERSION}-dev" libedit-dev libyaml-cpp-dev libboost-dev
--- ci/install_macos.sh
+++ ci/install_macos.sh
@@ -1,3 +1,9 @@
-brew install boost llvm@3.7
+if [ "${LLVM_VERSION}" = 'latest' ]; then
+  brew install llvm
+  export LLVM_VERSION=$(brew list --versions | sed -n 's/llvm \([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)/\1.\2/p')
+else
+  brew install "llvm@${LLVM_VERSION}"
+fi
+
+brew install boost
 brew install yaml-cpp --with-static-lib
-export PKG_CONFIG_PATH=/usr/local/Cellar/yaml-cpp/0.5.2/lib/pkgconfig 
