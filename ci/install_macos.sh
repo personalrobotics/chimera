465cde072840b0741898df334e1b2455a1eeb3ae
Michael Koval <mkoval@cs.cmu.edu>
2017-03-18 20:00:41 -0400

    Fix llvm-latest installation on macOS.

--- ci/install_macos.sh
+++ ci/install_macos.sh
@@ -1,9 +1,9 @@
 if [ "${LLVM_VERSION}" = 'latest' ]; then
-  brew install llvm
+  export LLVM_PACKAGE='llvm'
   export LLVM_VERSION=$(brew list --versions | sed -n 's/llvm \([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)/\1.\2/p')
 else
-  brew install "llvm@${LLVM_VERSION}"
+  export LLVM_PACKAGE="llvm@${LLVM_VERSION}"
 fi
 
-brew install boost
+brew install boost "${LLVM_PACKAGE}"
 brew install yaml-cpp --with-static-lib
