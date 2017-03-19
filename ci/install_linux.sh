522d5d23cb7b7f9d557e3bc1e5f330183389abd0
Michael Koval <mkoval@cs.cmu.edu>
2017-03-19 14:08:17 -0400

    Set bash flags on Linux.

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -1,6 +1,8 @@
+export LLVM_DIR="/usr/share/llvm-${LLVM_VERSION}/cmake/"
+
+set +ex
 sudo add-apt-repository -y "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-${LLVM_VERSION} main"
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-get -q update
 sudo apt-get -y install "llvm-${LLVM_VERSION}-dev" "llvm-${LLVM_VERSION}-tools" "libclang-${LLVM_VERSION}-dev" libedit-dev libyaml-cpp-dev libboost-dev
-
-export LLVM_DIR="/usr/share/llvm-${LLVM_VERSION}/cmake/"
+set -ex
