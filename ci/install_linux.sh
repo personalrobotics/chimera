0c47c821938b8b71abc730912816452ef5a02445
Jeongseok Lee <jslee02@gmail.com>
2017-05-22 10:32:58 -0400

    Install lcov for coverage test

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -7,3 +7,4 @@ sudo apt-get -y install "llvm-${LLVM_VERSION}-dev" "llvm-${LLVM_VERSION}-tools" 
 
 # Install test dependencies.
 sudo apt-get -y install libboost-python-dev
+sudo apt-get -y install lcov
