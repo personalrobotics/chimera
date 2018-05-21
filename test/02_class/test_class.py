a3dc70f409e04b5b00e909cc274109279c0f53c1
Jeongseok Lee <jslee02@gmail.com>
2018-05-21 16:57:05 -0700

    Start using unittest

--- test/02_class/test_class.py
+++ test/02_class/test_class.py
@@ -1,2 +1,25 @@
-import test_class_pybind11
-import test_class_boost_python
+import unittest
+
+from test_class_pybind11.test_nested_class import Pet as Pet_py11
+from test_class_boost_python.test_nested_class import Pet as Pet_bp
+
+
+class TestClass(unittest.TestCase):
+
+    def test_name_py11(self):
+        p = Pet_py11('Molly')
+        self.assertEqual(p.name, 'Molly')
+
+        p.setName('Charly')
+        self.assertEqual(p.name, 'Charly')
+
+    def test_name_pb(self):
+        p = Pet_bp('Molly')
+        self.assertEqual(p.name, 'Molly')
+
+        p.setName('Charly')
+        self.assertEqual(p.name, 'Charly')
+
+
+if __name__ == '__main__':
+    unittest.main()
