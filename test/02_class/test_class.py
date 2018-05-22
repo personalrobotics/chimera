2eabda8d96afc262cf512bd47e937bc51f2aed3d
Jeongseok Lee <jslee02@gmail.com>
2018-05-21 17:15:40 -0700

    [pybind11] Return value policy for class functions

--- test/02_class/test_class.py
+++ test/02_class/test_class.py
@@ -9,16 +9,20 @@ class TestClass(unittest.TestCase):
     def test_name_py11(self):
         p = Pet_py11('Molly')
         self.assertEqual(p.name, 'Molly')
+        self.assertEqual(p.getName(), 'Molly')
 
         p.setName('Charly')
         self.assertEqual(p.name, 'Charly')
+        self.assertEqual(p.getName(), 'Charly')
 
     def test_name_pb(self):
         p = Pet_bp('Molly')
         self.assertEqual(p.name, 'Molly')
+        self.assertEqual(p.getName(), 'Molly')
 
         p.setName('Charly')
         self.assertEqual(p.name, 'Charly')
+        self.assertEqual(p.getName(), 'Charly')
 
 
 if __name__ == '__main__':
