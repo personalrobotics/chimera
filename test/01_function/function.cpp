a89741b441f12098c11892762ce22d73722f7721
Pras Velagapudi <psigen@gmail.com>
2018-05-27 22:35:48 -0400

    Fixed minor errors that were preventing tests from passing.

--- test/01_function/function.cpp
+++ test/01_function/function.cpp
@@ -1,8 +1,12 @@
 #include "function.h"
 
-using namespace function::nested_function;
+namespace function {
+namespace nested_function {
 
-int add(int i, int j)
+int ::function::nested_function::add(int i, int j)
 {
     return i + j;
 }
+
+} // namespace nested_function
+} // namespace function
--- test/01_function/function.py
+++ test/01_function/function.py
@@ -4,26 +4,26 @@ from function_pybind11.nested_function import add as add_py11
 from function_pybind11.nested_function import inline_add as inline_add_py11
 
 from function_boost_python.nested_function import add as add_bp
-from function_boost_python.nested_function import inline_add as add_bp
+from function_boost_python.nested_function import inline_add as inline_add_bp
 
 
 class TestFunction(unittest.TestCase):
 
     def test_function_py11(self):
-        self.assertEqual(add_py11, 3)
+        self.assertEqual(add_py11(), 3)
         self.assertEqual(add_py11(3, 4), 7)
         self.assertEqual(add_py11(i=5, j=6), 11)
 
-        self.assertEqual(inline_add_py11, 3)
+        self.assertEqual(inline_add_py11(), 3)
         self.assertEqual(inline_add_py11(3, 4), 7)
         self.assertEqual(inline_add_py11(i=5, j=6), 11)
 
     def test_function_bp(self):
-        self.assertEqual(add_bp, 3)
+        self.assertEqual(add_bp(), 3)
         self.assertEqual(add_bp(3, 4), 7)
         self.assertEqual(add_bp(i=5, j=6), 11)
 
-        self.assertEqual(inline_add_bp, 3)
+        self.assertEqual(inline_add_bp(), 3)
         self.assertEqual(inline_add_bp(3, 4), 7)
         self.assertEqual(inline_add_bp(i=5, j=6), 11)
 
