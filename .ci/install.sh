f012f6c20bf19c29c599fbd42209078e10ba5a79
Jeongseok Lee <jslee02@gmail.com>
2018-09-15 00:46:40 -0700

    Fix path to script

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 
-if [ "${TRAVIS_OS_NAME}" = "linux" ]; then . install_linux.sh; fi
-if [ "${TRAVIS_OS_NAME}" = "osx"   ]; then . install_macos.sh; fi
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then . .ci/install_linux.sh; fi
+if [ "${TRAVIS_OS_NAME}" = "osx"   ]; then . .ci/install_macos.sh; fi
