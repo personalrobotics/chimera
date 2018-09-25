52779ec899b567ffad425f56bbc70dcfd1d5873e
Jeongseok Lee <jslee02@gmail.com>
2018-09-25 08:17:00 -0700

    Adding enum class tests

--- test/04_enumeration/enumeration.h
+++ test/04_enumeration/enumeration.h
@@ -16,5 +16,57 @@ struct Animal
   Type type;
 };
 
+enum GlobalEnum
+{
+  GE_TYPE1 = 0,
+  GE_TYPE2
+};
+
+enum class GlobalEnumClass
+{
+  TYPE1 = 0,
+  TYPE2
+};
+
+struct MainStruct
+{
+  enum Type
+  {
+    Dog = 0,
+    Cat
+  };
+
+  enum EnumInStruct
+  {
+    ICE_TYPE1 = 0,
+    ICE_TYPE2
+  };
+
+  enum EnumClassInStruct
+  {
+    TYPE1 = 0,
+    TYPE2
+  };
+
+  struct NestedStruct
+  {
+    enum EnumInStruct
+    {
+      ICE_TYPE1 = 0,
+      ICE_TYPE2
+    };
+
+    enum EnumClassInStruct
+    {
+      TYPE1 = 0,
+      TYPE2
+    };
+
+    NestedStruct() = default;
+  };
+
+  MainStruct() = default;
+};
+
 } // namespace nested_namespace
 } // namespace chimera_test
