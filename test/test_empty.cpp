7e10d989b249a22620fd766a741b7fb47b2e3e71
Jeongseok Lee <jslee02@gmail.com>
2019-01-19 00:15:11 -0800

    Adding emulator and some basic tests

---
+++ test/test_empty.cpp
@@ -0,0 +1,11 @@
+#include "emulator.h"
+#include "chimera/frontend_action.h"
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Tooling/Tooling.h>
+#include <gtest/gtest.h>
+
+using namespace chimera;
+using namespace chimera::test;
+
+//==============================================================================
+TEST(Empty, Empty) {}
