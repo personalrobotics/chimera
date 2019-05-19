9e6b866b95078be1ebf910827678fe39b7fe072c
Jeongseok Lee <jslee02@users.noreply.github.com>
2019-05-19 08:40:24 -0700

    Reflect upstream update of cling_utils_AST (#261)

--- cmake/chimeraFindLLVM.cmake
+++ cmake/chimeraFindLLVM.cmake
@@ -31,6 +31,7 @@ else()
 endif()
 
 # Check if LLVM is compatible with Chimera
+# LLVM >= 7 is disabled: https://github.com/personalrobotics/chimera/issues/222
 set(COMPATIBLE_LLVM_VERSIONS 3.6 3.9 6.0)
 set(LLVM_VERSION_MAJOR_MINOR ${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR})
 set(FOUND_COMPATIBLE_LLVM FALSE)
