5e272b45162e32799d827a6fb26a0c2609231658
Jeongseok Lee <jslee02@users.noreply.github.com>
2020-04-05 10:09:03 -0700

    Create a dedicated function to render top-level mstch (#269)
    * Create a dedicated function to render top-level mstch
    
    * Pass compiled config by value to Visitor
    
    * Remove unnecessary header inclusion

--- src/consumer.cpp
+++ src/consumer.cpp
@@ -15,9 +15,15 @@ void chimera::Consumer::HandleTranslationUnit(ASTContext &context)
 {
     // Use the current translation unit to resolve the YAML configuration.
     chimera::Configuration &config = chimera::Configuration::GetInstance();
-    chimera::Visitor visitor(ci_, config.Process(ci_));
+
+    std::unique_ptr<chimera::CompiledConfiguration> compiled_config
+        = config.Process(ci_);
+    chimera::Visitor visitor(ci_, *compiled_config);
 
     // We can use ASTContext to get the TranslationUnitDecl, which is
     // a single Decl that collectively represents the entire source file.
     visitor.TraverseDecl(context.getTranslationUnitDecl());
+
+    // Render the top-level mstch template
+    compiled_config->Render();
 }
