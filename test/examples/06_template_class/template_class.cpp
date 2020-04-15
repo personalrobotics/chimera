277299d22564519f30c8f5348de0317f7b70a20f
Jeongseok Lee <jslee02@gmail.com>
2020-04-15 02:13:41 -0700

    Add explicit template class instantiation example

---
+++ test/examples/06_template_class/template_class.cpp
@@ -0,0 +1,10 @@
+#include "template_class.h"
+
+namespace chimera_test
+{
+
+//==============================================================================
+// Explicit instantiation definition
+template class Vector<double>;
+
+} // namespace chimera_test
