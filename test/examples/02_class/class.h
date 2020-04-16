f5d01b4cc7ff4b19ffe83c44834132be1ff3c8d6
Jeongseok Lee <jslee02@gmail.com>
2020-04-16 14:47:38 -0700

    Add static method example -- failure case

--- test/examples/02_class/class.h
+++ test/examples/02_class/class.h
@@ -99,4 +99,28 @@ class ClassInDetail
 
 } // namespace detail
 
+namespace test1
+{
+
+class Integer
+{
+public:
+    Integer(int val) : m_val(val)
+    {
+    }
+    int add(int a)
+    {
+        return a + m_val;
+    }
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+private:
+    int m_val;
+};
+
+} // namespace test1
+
 } // namespace chimera_test
--- test/examples/02_class/class.py
+++ test/examples/02_class/class.py
@@ -10,92 +10,60 @@ except:
 import class_boost_python as boost
 
 
-class TestFunction(unittest.TestCase):
+class TestClass(unittest.TestCase):
 
-    def test_function_py11(self):
-        if not has_pybind11:
-            return
-
-        dog = py11.Dog()
+    def _test_class(self, binding):
+        dog = binding.Dog()
         self.assertEqual(dog.type(), 'Dog')
         self.assertEqual(dog.pure_virtual_type(), 'Dog')
-        self.assertEqual(py11.Dog.static_type(), 'Dog')
+        self.assertEqual(binding.Dog.static_type(), 'Dog')
 
-        husky = py11.Husky()
+        husky = binding.Husky()
         self.assertEqual(husky.type(), 'Husky')
         self.assertEqual(husky.pure_virtual_type(), 'Husky')
 
-        strong_husky = py11.StrongHusky()
+        strong_husky = binding.StrongHusky()
         self.assertEqual(strong_husky.type(), 'StrongHusky')
         self.assertEqual(strong_husky.pure_virtual_type(), 'StrongHusky')
 
-        default_args = py11.DefaultArguments()
+        default_args = binding.DefaultArguments()
         self.assertEqual(default_args.add(), 3)
         self.assertEqual(default_args.add(2), 4)
         self.assertEqual(default_args.add(2, 3), 5)
 
-        self.assertEqual(py11.StaticFields.m_static_readonly_type, 'static readonly type')
-        self.assertEqual(py11.StaticFields.m_static_readwrite_type, 'static readwrite type')
-        py11.StaticFields.m_static_readwrite_type = 'new type'
-        self.assertEqual(py11.StaticFields.m_static_readwrite_type, 'new type')
-        self.assertEqual(py11.StaticFields.static_type(), 'static type')
-
-        non_pub_param_in_ctr = py11.NonPublicParamInConstructor('Herb')
-        self.assertEqual(non_pub_param_in_ctr.m_name, 'Herb')
-
-        # Check if the main class and the nested class can be created w/o errors
-        mc = py11.MainClass()
-        nc = py11.MainClass.NestedClass()
-
-        # MainClass shouldn't be a module
-        self.assertFalse(inspect.ismodule(py11.MainClass))
-        self.assertTrue(inspect.isclass(py11.MainClass))
-        self.assertTrue(inspect.isclass(py11.MainClass.NestedClass))
-
-        import class_pybind11
-        self.assertFalse(hasattr(class_pybind11, 'detail'))
-
-    def test_function_bp(self):
-        dog = boost.Dog()
-        self.assertEqual(dog.type(), 'Dog')
-        self.assertEqual(dog.pure_virtual_type(), 'Dog')
-        self.assertEqual(boost.Dog.static_type(), 'Dog')
-
-        husky = boost.Husky()
-        self.assertEqual(husky.type(), 'Husky')
-        self.assertEqual(husky.pure_virtual_type(), 'Husky')
-
-        strong_husky = boost.StrongHusky()
-        self.assertEqual(strong_husky.type(), 'StrongHusky')
-        self.assertEqual(strong_husky.pure_virtual_type(), 'StrongHusky')
-
-        default_args = boost.DefaultArguments()
-        self.assertEqual(default_args.add(), 3)
-        self.assertEqual(default_args.add(2), 4)
-        self.assertEqual(default_args.add(2, 3), 5)
-
-        self.assertEqual(boost.StaticFields.m_static_readonly_type, 'static readonly type')
-        self.assertEqual(boost.StaticFields.m_static_readwrite_type, 'static readwrite type')
+        self.assertEqual(binding.StaticFields.m_static_readonly_type, 'static readonly type')
+        self.assertEqual(binding.StaticFields.m_static_readwrite_type, 'static readwrite type')
         # TODO(JS): Boost.Python doesn't work for readwrite static field (see: #194)
-        # boost.StaticFields.m_static_readwrite_type = 'new type'
-        # self.assertEqual(boost.StaticFields.m_static_readwrite_type, 'new type')
-        self.assertEqual(boost.StaticFields.static_type(), 'static type')
+        if has_pybind11 and binding is py11:
+            binding.StaticFields.m_static_readwrite_type = 'new type'
+            self.assertEqual(binding.StaticFields.m_static_readwrite_type, 'new type')
+        self.assertEqual(binding.StaticFields.static_type(), 'static type')
 
-        non_pub_param_in_ctr = boost.NonPublicParamInConstructor('Herb')
+        non_pub_param_in_ctr = binding.NonPublicParamInConstructor('Herb')
         self.assertEqual(non_pub_param_in_ctr.m_name, 'Herb')
 
         # Check if the main class and the nested class can be created w/o errors
-        mc = boost.MainClass()
-        nc = boost.MainClass.NestedClass()
+        mc = binding.MainClass()
+        nc = binding.MainClass.NestedClass()
 
         # MainClass shouldn't be a module
-        self.assertFalse(inspect.ismodule(boost.MainClass))
-        self.assertTrue(inspect.isclass(boost.MainClass))
-        self.assertTrue(inspect.isclass(boost.MainClass.NestedClass))
+        self.assertFalse(inspect.ismodule(binding.MainClass))
+        self.assertTrue(inspect.isclass(binding.MainClass))
+        self.assertTrue(inspect.isclass(binding.MainClass.NestedClass))
 
-        import class_boost_python
-        self.assertFalse(hasattr(class_boost_python, 'detail'))
+        self.assertFalse(hasattr(binding, 'detail'))
 
+        # Call static method
+        self.assertEqual(binding.test1.Integer.Add(1, 2), 3)
+        
+        # Call instance method
+        i = binding.test1.Integer(1)
+        self.assertEqual(i.add(2), 3)
+
+    def test_class(self):
+        self._test_class(boost)
+        if has_pybind11:
+            self._test_class(py11)
 
 if __name__ == '__main__':
     unittest.main()
