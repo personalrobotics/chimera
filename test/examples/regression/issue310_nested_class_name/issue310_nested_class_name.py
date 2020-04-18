e36ec127a6e9917b5d1f530b64cdacf936b9668e
Jeongseok Lee <jslee02@gmail.com>
2020-04-18 00:19:49 -0700

    Add regression test for #310

---
+++ test/examples/regression/issue310_nested_class_name/issue310_nested_class_name.py
@@ -0,0 +1,22 @@
+import unittest
+
+import issue310_nested_class_name_boost_python as boost
+
+
+class TestIssue310(unittest.TestCase):
+
+    def _import_py(self):
+        import issue310_nested_class_name_pybind11
+
+    def _test_issue310(self, binding):
+        self.assertTrue(hasattr(binding.Base, 'Option'))
+        self.assertTrue(hasattr(binding.Derived, 'Option'))
+
+    def test_issue310(self):
+        self.assertRaises(RuntimeError, self._import_py())
+
+        self._test_issue310(boost)
+
+
+if __name__ == '__main__':
+    unittest.main()
