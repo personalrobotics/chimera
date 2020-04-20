20c4861f8516c7f5e8524bafebeb83d2e94c0b47
Jeongseok Lee <jslee02@users.noreply.github.com>
2020-04-20 00:01:48 -0700

    Update issue310_nested_class_name.py

--- test/examples/regression/issue310_nested_class_name/issue310_nested_class_name.py
+++ test/examples/regression/issue310_nested_class_name/issue310_nested_class_name.py
@@ -5,16 +5,12 @@ import issue310_nested_class_name_boost_python as boost
 
 class TestIssue310(unittest.TestCase):
 
-    def _import_py(self):
-        import issue310_nested_class_name_pybind11
-
     def _test_issue310(self, binding):
         self.assertTrue(hasattr(binding.Base, 'Option'))
         self.assertTrue(hasattr(binding.Derived, 'Option'))
 
     def test_issue310(self):
-        self.assertRaises(ImportError, self._import_py())
-
+        self.assertRaises(ImportError, __import__('issue310_nested_class_name_pybind11'))
         self._test_issue310(boost)
 
 
