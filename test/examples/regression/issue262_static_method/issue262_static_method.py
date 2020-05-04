ecadd41b9dfd46e181b5632c2c74379f81ae9875
Jeongseok Lee <jslee02@users.noreply.github.com>
2020-05-04 01:42:56 -0700

    Skip static methods that overload instance methods (#321)

---
+++ test/examples/regression/issue262_static_method/issue262_static_method.py
@@ -0,0 +1,26 @@
+import unittest
+
+import issue262_static_method_boost_python as boost
+import issue262_static_method_pybind11 as py11
+
+
+class TestIssue262(unittest.TestCase):
+
+    def _test_issue262(self, binding):
+        # Should be skipped as this static method overloads instance method
+        with self.assertRaises(TypeError):
+            binding.Integer.add(1, 2)
+
+        self.assertEqual(binding.Integer.add_static(1, 2), 3)
+
+        # Call instance method
+        i = binding.Integer(1)
+        self.assertEqual(i.add(2), 3)
+
+    def test_issue262(self):
+        self._test_issue262(boost)
+        self._test_issue262(py11)
+
+
+if __name__ == '__main__':
+    unittest.main()
