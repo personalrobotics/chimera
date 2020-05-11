5d55150f3b56ce55aa4dce07f4c68420c50937f0
Jeongseok Lee <jeoslee@amazon.com>
2020-05-10 21:47:00 -0700

    Add unit test that should pass once #328 is resolved

--- test/examples/regression/issue328_dependent_name/issue328_dependent_name.py
+++ test/examples/regression/issue328_dependent_name/issue328_dependent_name.py
@@ -7,6 +7,8 @@ import issue328_dependent_name_boost_python as boost
 class TestIssue328(unittest.TestCase):
 
     def _test_issue328(self, binding):
+        # TODO: The next line should be uncommented once #328 is resolved.
+        # self.assertEqual(binding.VectorScalar, float)
         pass
 
     def test_issue328(self):
