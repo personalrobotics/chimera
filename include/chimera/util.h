5590e587ecdfd2749a992cdfe033d79228671f65
Jeongseok Lee <jslee02@users.noreply.github.com>
2020-06-04 16:23:08 -0700

    Update util.h

--- include/chimera/util.h
+++ include/chimera/util.h
@@ -15,11 +15,13 @@
   LLVM_VERSION_PATCH >= z))))
 // clang-format on
 
+/** Macro to suppress the unused variable warnings. */
 #define CHIMERA_UNUSED(x)                                                      \
     do                                                                         \
     {                                                                          \
         (void)(x);                                                             \
     } while (0)
+// TODO: This macro should be replaced with [[maybe_unused]] once C++17 available
 
 namespace chimera
 {
